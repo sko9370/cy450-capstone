60fd6061843934ac870d776467c8a69794f30065
sko9370 <sko9370@ymail.com>
2019-04-13 08:15:17 -0700

    update stopDisable.sh

--- firewall.sh
+++ firewall.sh
@@ -2,6 +2,21 @@ echo "what is the ip of this computer?"
 
 read ip
 
+# for web server
+if [ $ip = "10.82.2.140" ]; then
+	sudo ufw allow in proto tcp from any to any port 80,443
+fi
+
+# for internal dns
+if [ $ip = "10.82.2.5" ]; then
+	sudo ufw allow in proto udp from any to any port 53
+fi
+
+# for external dns
+if [ $ip = "10.82.2.130" ]; then
+	sudo ufw allow in proto udp from any to any port 53
+fi
+
 sudo ufw allow out from $ip to 10.82.2.0/24 port 22
 
 sudo ufw allow in from 10.82.2.0/24 to $ip port 22
--- stopDisable.sh
+++ stopDisable.sh
@@ -1,11 +1,24 @@
+echo "what is the ip of this computer?"
+
+read ip
+
+# for web server
+if [ $ip != "10.82.2.140" ]; then	
+	sudo systemctl stop apache2.service
+	sudo systemctl disable apache2.service
+fi
+
+# for internal and external dns
+if [ $ip != "10.82.2.140" ] || [ $ip != "10.82.2.5" ]; then
+	sudo systemctl stop bind9.service
+	sudo systemctl disable bind9.service
+fi
+
 sudo systemctl stop mysql.service
 sudo systemctl disable mysql.service
 
 sudo systemctl stop bind9.service
 sudo systemctl disable bind9.service
-
-sudo systemctl stop apache2.service
-sudo systemctl disable apache2.service
 
 sudo systemctl stop bluetooth.service
 sudo systemctl disable bluetooth.service
